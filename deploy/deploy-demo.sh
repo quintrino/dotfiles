624738830390d156ca93b481cd4d13ab87aa5691
Nick Wolf <nickwolf@marketplacer.com>
2018-08-13 21:36:42 +1000

    Update deploy with environment for brewfiles

--- deploy/deploy-demo.sh
+++ deploy/deploy-demo.sh
@@ -1,10 +1,2 @@
-cd dotfiles/brew
-if [ "$WORK" == 1 ];
-  then cd work
-  brew bundle
-fi
-if [ "$HOME" == 1 ];
-  then cd home
-  brew bundle
-fi
+
 echo 'EOF'
--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,15 @@
+# sudo curl -fsSL  https://raw.githubusercontent.com/quintrino/dotfiles/master/deploy/deploy.sh | WORK=1 bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 git clone https://github.com/quintrino/dotfiles.git
 cd dotfiles/brew
 brew bundle
+if [ "$WORK" == 1 ];
+  then cd work
+  brew bundle
+fi
+if [ "$HOME" == 1 ];
+  then cd home
+  brew bundle
+fi
+cd
 
