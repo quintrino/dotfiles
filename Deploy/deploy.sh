76da983539a07868290d33b4cca3215aa6d011da
Nick Wolf <nickwolf@marketplacer.com>
2018-08-13 19:33:05 +1000

    Add Work specific brewfiles

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -1,1 +1,5 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+git clone https://github.com/quintrino/dotfiles.git
+cd dotfiles/Brew
+brew bundle
+
