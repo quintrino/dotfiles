37a89db98bab43d528ab0766c11497da6e7eeb0f
Nick Wolf <git@nickwolf.com.au>
2018-10-10 12:55:32 +1100

    Add iTerm config to dotfiles and deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -100,3 +100,9 @@ defaults write com.apple.menuextra.clock "DateFormat" 'EEE HH:mm'
 
 # Reset SystemUIServer
 killall -KILL SystemUIServer
+
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iTerm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
