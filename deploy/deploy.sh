12f69e4603ccb57045d8875d607aedaee2c0c2f3
Nick Wolf <git@nickwolf.com.au>
2019-03-29 16:18:40 +1100

    Change source functions link to deploy directory

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 git clone https://github.com/quintrino/dotfiles.git .dotfiles
 
-source $HOME/.dotfiles/deploy_functions.sh
+source $HOME/.dotfiles/deploy/deploy_functions.sh
 source $HOME/.dotfiles/environment
 
 install_config_folders
