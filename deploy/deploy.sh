93378ba6ab7e2fedd31639f297e647e348e3ecd5
Nick Wolf <git@nickwolf.com.au>
2019-05-14 17:14:22 +1000

    Remove sudo command from deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,4 @@
 # sudo curl -fsSL  https://raw.githubusercontent.com/quintrino/dotfiles/master/deploy/deploy.sh | WORK=1 bash
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 git clone https://github.com/quintrino/dotfiles.git .dotfiles
 
