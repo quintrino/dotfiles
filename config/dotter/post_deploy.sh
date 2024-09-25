9a87676896fef3f189bb9e8d70a5c198be240dc8
Nick Wolf <git@nickwolf.com.au>
2024-09-25 11:39:20 +1000

    Add post_deploy to dotter

---
+++ config/dotter/post_deploy.sh
@@ -0,0 +1,16 @@
+echo "\033[1;31mRunning Post Deploy Hooks\033[0m"
+echo "Generating symlinks"
+echo "Symlinking apps/vlc/vlcrc"
+ln -sf ~/.dotfiles/apps/vlc/vlcrc ~/Library/Preferences/org.videolan.vlc/vlcrc
+echo "Symlinking apps/vs-code/settings.vsrc"
+ln -sf ~/.dotfiles/apps/vs-code/settings.vsrc ~/Library/Application\ Support/Code/User/settings.json
+echo "Symlinking apps/vs-code/settings.vsrc"
+ln -sf ~/.dotfiles/apps/vs-code/keys.vsrc ~/Library/Application\ Support/Code/User/keybindings.json
+if [ -d $HOME/Proton ]; then
+      echo "/Proton/ detected"
+      echo "Generating symlinks"
+      echo "Symlinking Devices/Mac/private.yml"
+      ln -sf ~/Proton/Sync/Devices/Mac/private.yml $XDG_CONFIG_HOME/espanso/match/private.yml
+else
+  echo "Proton Drive not found"
+fi
